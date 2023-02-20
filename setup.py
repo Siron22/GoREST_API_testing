e8ebff2a5de5f3184ed2e2c720c69cfe35a8fd7b
Siron22 <s0949528587@gmail.com>
2023-02-20 20:57:42 +0100

    edit setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name='GoREST_API_testing',
       version='1.0',
       description='Little framework with automated tests for GoREST service',
-      packages=['api-tests'],
+      packages=['api_tests'],
       install_requires=['pytest',
                         'selenium',
                         'webdriver-manager',
