fc459f492fdcf4931835587ab69308ed5b680160
Siron22 <s0949528587@gmail.com>
2023-02-20 10:25:02 +0100

    homework commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name='GoREST_API_testing',
       version='1.0',
       description='Little framework with automated tests for GoREST service',
-      packages=['to_delete'],
+      packages=['api-tests'],
       install_requires=['pytest',
                         'selenium',
                         'webdriver-manager',
